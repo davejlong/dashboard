5e38efae470cd911562f78c69af005d10cd4db04
Dave Long <dlong@cagedata.com>
2019-09-27 12:12:18 -0400

    Initial commit

---
+++ Get-Atera.ps1
@@ -0,0 +1,25 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string] $Endpoint,
+    [Parameter()]
+    [string] $Method="GET",
+    [Parameter()]
+    [string] $ApiKey
+)
+
+function New-AteraRequest($Uri, $Method, $Headers, $ResultSet=@()) {
+    $Result = Invoke-RestMethod -Uri $Uri -Method $Method -Headers $Headers
+    $ResultSet = $ResultSet + $Result.items
+    if($Result.page -lt $Result.totalPages) {
+        Write-Host "Going around again (Page $($Result.page) of $($Result.totalPages))"
+        New-AteraRequest -Uri $Result.nextLink -Method $Method -Headers $Headers -ResultSet $ResultSet
+    } else { return $ResultSet }
+}
+
+$ApiUrl = "https://app.atera.com/api/v3"
+$Uri = "$($ApiUrl)$($Endpoint)?itemsInPage=50"
+
+$Headers = @{'X-API-KEY' = "$($ApiKey)"}
+
+return (New-AteraRequest -Uri $Uri -Method $Method -Headers $Headers)
---
+++ README.md
@@ -0,0 +1,13 @@
+# Cage Data Dashboard
+
+## Installation
+
+```
+Install-Module UniversalDashboard.Community
+```
+
+## Starting
+
+```
+Start-Dashboard.ps1 -AteraAPIKey "[YOUR API KEY FROM ATERA]"
+```
