081219caee3b09df01f3559939ae9e2b871d96fb
Dave Long <dlong@cagedata.com>
2020-05-01 11:41:26 -0400

    Renamed start script

--- Start-Dashboard.ps1
+++
@@ -1,6 +0,0 @@
-Import-Module ".\dashboard.psm1" -Force
-
-# Change to your email address to filter "My Tickets" and "Open Tickets"
-Set-TechnicianEmail -Email 'dlong@cagedata.com'
-
-Start-UDDashboard -Dashboard (New-Dashboard) -Endpoint (Get-Endpoints) -Port 10001
