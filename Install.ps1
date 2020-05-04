27831770e6e68983bf0607d458b4acd6e0552292
David Long <dlong@cagedata.com>
2020-05-04 10:22:38 -0400

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -13,7 +13,7 @@ $Email = Read-Host "Enter your email"
 Write-Host "Setting email in dashboard.ps1"
 $content = Get-Content $PSScriptRoot/dashboard.ps1.tmpl `
 | ForEach-Object { $_ -replace "<<EMAIL>>", $Email } `
-$content | Set-Content $PSScriptRoot/dashboard.ps1
+| $content | Set-Content $PSScriptRoot/dashboard.ps1
 
 Write-Host "Dashboard is installed."
 Write-Host "To start the dashboard run ./dashboard.ps1"
